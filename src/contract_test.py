e0a090bfbb803d3430b9a85c2e4f0cfaa71a30e1
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-09-15 00:01:02 +0800

    Tests

--- src/contract_test.py
+++ src/contract_test.py
@@ -3,28 +3,59 @@ import pytest
 
 from starkware.starknet.testing.starknet import Starknet
 
+def str_to_felt( str ):
+	return int( str.encode('utf-8').hex(), 16 );
+
+OWNER_ADDR = 0x70AEBF10E9735F92CA6EDC49A9D6F3774FFAFE92;
+ADDR1 = 0x32429A3EB84A66A83A038E66540042F5864E51A0;
+ADDR2 = 0x8421A0832429A64422F54A66A83A038E53EBE665;
 # The path to the contract source code.
-CONTRACT_FILE = os.path.join(
-    os.path.dirname(__file__), "contract.cairo")
+CONTRACT_FILE = os.path.join(os.path.dirname(__file__), "contract.cairo")
 
-# The testing library uses Python's asyncio. So the following
-# decorator and the ``async`` keyword are needed.
+async def setupContract( constructor_calldata = [] ):
+	# Create a new Starknet class that simulates the StarkNet
+	# system.
+	starknet = await Starknet.empty()
+
+	# Deploy the contract.
+	return await starknet.deploy(
+		source=CONTRACT_FILE,
+		constructor_calldata=constructor_calldata,
+	)
+
 @pytest.mark.asyncio
-async def test_increase_balance():
-    # Create a new Starknet class that simulates the StarkNet
-    # system.
-    starknet = await Starknet.empty()
+async def test_constructor_takes_one_param():
+	await setupContract( [OWNER_ADDR] )
 
-    # Deploy the contract.
-    contract = await starknet.deploy(
-        source=CONTRACT_FILE,
-    )
+@pytest.mark.asyncio
+async def test_owner_has_role_roles_manager():
+	contract = await setupContract( [OWNER_ADDR] )
+	exec = await contract.has_role( address=OWNER_ADDR, role=str_to_felt( 'roles_manager' ) ).call()
+	assert exec.result == (1,)
 
-    # Invoke increase_balance() twice.
-    await contract.increase_balance(amount=10).execute()
-    await contract.increase_balance(amount=15).execute()
+@pytest.mark.asyncio
+async def test_add_role_is_protected():
+	contract = await setupContract( [OWNER_ADDR] )
 
-    # Check the result of get_balance().
-    execution_info = await contract.get_balance().call()
-    assert execution_info.result == (25,)
-    print( execution_info.result )
+	# add roles_manager role for addr1, should fail.
+	await contract.add_role( address=ADDR1, role=str_to_felt( 'roles_manager' ) ).execute()
+
+	# addr1 should not have roles_manager role
+	exec = await contract.has_role( address=ADDR1, role=str_to_felt( 'roles_manager' ) ).call()
+	assert exec.result == (0,)
+
+# @pytest.mark.asyncio
+# async def test_owner_can_add_addr():
+# 	contract = await setupContract( [OWNER_ADDR] )
+
+# 	# addr1 does not has roles_manager role
+# 	exec = await contract.has_role( address=ADDR1, role=str_to_felt( 'roles_manager' ) ).call()
+# 	assert exec.result == (0,)
+
+# 	# add roles_manager role for addr1
+# 	await contract.add_role( address=ADDR1, role=str_to_felt( 'roles_manager' ) ).execute()
+
+# 	# addr1 now has roles_manager role
+# 	exec = await contract.has_role( address=ADDR1, role=str_to_felt( 'roles_manager' ) ).call()
+# 	assert exec.result == (1,)
+
